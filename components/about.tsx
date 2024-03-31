5fd36ddf519624059655e36abfb0b89e93362295
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2024-03-31 19:00:28 +0530

    feat: updating about section

--- components/about.tsx
+++ components/about.tsx
@@ -19,30 +19,24 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
+        A back-end developer who just can't get enough of{" "}
+        <span className="font-medium">technology.</span> Started off tinkering
+        with front-end stuff, but soon found my groove in the{" "}
+        <span className="font-medium">back-end world.</span>{" "}
+        <span className="italic">I love diving into tricky problems and</span>{" "}
+        coming up with smart solutions,<span className="underline">love</span>
+        to make web applications run smoothly. My core stack is{" "}
       </p>
 
       <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        <span className="italic">
+          When I'm not coding, you'll probably find me typing away on my blog,
+        </span>{" "}
+        sharing what I've learned and hopefully making tech less intimidating
+        for everyone.{" "}
+        <span className="font-medium">
+          Let's navigate this digital universe together!
+        </span>
       </p>
     </motion.section>
   );
